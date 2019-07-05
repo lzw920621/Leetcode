49b34c6a3b44670c9e1747a801680e90658dade3
lzw920621 <1010616217@qq.com>
2019-07-05 16:21:16 +0800

    更新

---
+++ _125_验证回文字串/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _125_验证回文字串
+{
+    /*
+    给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+    说明：本题中，我们将空字符串定义为有效的回文串。
+
+    示例 1:
+
+    输入: "A man, a plan, a canal: Panama"
+    输出: true
+
+    示例 2:
+
+    输入: "race a car"
+    输出: false
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/valid-palindrome
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            if(s==null||s=="")
+            {
+                return true;
+            }
+            s = s.ToLower();
+            List<char> list = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s[i]>=48 && s[i]<=57)//数字
+                {
+                    list.Add(s[i]);
+                }                
+                else if(s[i] >= 97 && s[i] <= 122)//小写字母
+                {
+                    list.Add(s[i]);
+                }
+            }
+
+            if(list.Count>0)
+            {
+                for (int i = 0; i < list.Count/2; i++)
+                {
+                    if (list[i] != list[list.Count - 1 - i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ 字符串中第一个唯一字符/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 字符串中第一个唯一字符
+{
+    /*
+    给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+    案例:
+
+    s = "leetcode"
+    返回 0.
+
+    s = "loveleetcode",
+    返回 2.
+
+ 
+
+    注意事项：您可以假定该字符串只包含小写字母。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(dic.ContainsKey(s[i]))
+                {
+                    dic[s[i]]++;
+                }
+                else
+                {
+                    dic.Add(s[i], 1);
+                }
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(dic[s[i]]==1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int FirstUniqChar1(string s)
+        {
+            var nums = new int[26];
+            foreach (var ch in s)
+            {
+                var key = ch - 'a';
+                nums[key]++;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                var key = s[i] - 'a';
+                if (nums[key] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
