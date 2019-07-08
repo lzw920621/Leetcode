ef86c525e9d57b274317846b0fdcd8a98877c9cd
lzw920621 <1010616217@qq.com>
2019-07-08 10:02:50 +0800

    更新

---
+++ _896_单调序列/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _896_单调序列
+{
+    /*
+    如果数组是单调递增或单调递减的，那么它是单调的。
+
+    如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+
+    当给定的数组 A 是单调数组时返回 true，否则返回 false。
+
+ 
+
+    示例 1：
+
+    输入：[1,2,2,3]
+    输出：true
+
+    示例 2：
+
+    输入：[6,5,4,4]
+    输出：true
+
+    示例 3：
+
+    输入：[1,3,2]
+    输出：false
+
+    示例 4：
+
+    输入：[1,2,4,5]
+    输出：true
+
+    示例 5：
+
+    输入：[1,1,1]
+    输出：true
+
+ 
+
+    提示：
+
+        1 <= A.length <= 50000
+        -100000 <= A[i] <= 100000
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/monotonic-array
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = IsMonotonic(new int[] { 2, 2, 2, 1, 4, 5 });
+        }
+
+        public static bool IsMonotonic(int[] A)
+        {
+            bool up = true;
+            bool down = true;
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] > A[i - 1])
+                {
+                    down = false;
+                }
+                else if (A[i] < A[i - 1])
+                {
+                    up = false;
+                }
+            }
+            return up || down;
+        }
+    }
+}
---
+++ _917_仅仅反转字母/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _917_仅仅反转字母
+{
+    /*
+    给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+
+    示例 1：
+
+    输入："ab-cd"
+    输出："dc-ba"
+
+    示例 2：
+
+    输入："a-bC-dEf-ghIj"
+    输出："j-Ih-gfE-dCba"
+
+    示例 3：
+
+    输入："Test1ng-Leet=code-Q!"
+    输出："Qedo1ct-eeLg=ntse-T!"
+
+ 
+
+    提示：
+
+        S.length <= 100
+        33 <= S[i].ASCIIcode <= 122 
+        S 中不包含 \ or "
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/reverse-only-letters
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = ReverseOnlyLetters("Test1ng-Leet=code-Q!");
+        }
+
+        public static string ReverseOnlyLetters(string S)
+        {
+            char[] array = new char[S.Length];
+            int left = 0, right = S.Length - 1;
+            
+            while (left <= right)
+            {
+                while ((S[left] >= 33 && S[left]<=64)||(S[left]>=91&&S[left]<=96))//不是字母
+                {
+                    array[left] = S[left];
+                    left++;         
+                    if(left>=S.Length)
+                    {
+                        break;
+                    }
+                }
+                while ((S[right] >= 33 && S[right] <= 64) || (S[right] >= 91 && S[right] <= 96))//不是字母
+                {
+                    array[right] = S[right];
+                    right--;
+                    if(right<0)
+                    {
+                        break;
+                    }
+                }
+                if(left<=right)
+                {                   
+                    array[left] = S[right];
+                    array[right] = S[left];
+                    left++;
+                    right--;
+                }                
+            }
+            return new string(array);
+        }
+    }
+}
