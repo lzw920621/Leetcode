6153f774e2c06a965d66fee06a5e40765aba3c19
lzw920621 <1010616217@qq.com>
2019-07-30 15:56:14 +0800

    更新

---
+++ _107_二叉树的层次遍历II/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _107_二叉树的层次遍历II
+{
+    /*
+    给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+    例如：
+    给定二叉树 [3,9,20,null,null,15,7],
+
+        3
+       / \
+      9  20
+        /  \
+       15   7
+
+    返回其自底向上的层次遍历为：
+
+    [
+      [15,7],
+      [9,20],
+      [3]
+    ]
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            if (root == null) return list;
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            TreeNode current ;
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                int leveCount = queue.Count();//当前层的节点数
+                List<int> subList = new List<int>();
+                for (int i = 0; i < leveCount; i++)
+                {
+                    current = queue.Dequeue();
+                    subList.Add(current.val);
+
+                    if (current.left != null)
+                    {
+                        queue.Enqueue(current.left);
+                    }
+                    if (current.right != null)
+                    {
+                        queue.Enqueue(current.right);
+                    }
+                }
+                
+                list.Add(subList);
+            }
+            //翻转列表
+            for (int i = 0; i < list.Count/2; i++)
+            {
+                IList<int> temp = list[i];
+                list[i] = list[list.Count - 1 - i];
+                list[list.Count - 1 - i] = temp;
+            }
+            return list;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
