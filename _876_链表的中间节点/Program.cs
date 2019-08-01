de8dff884338d4c1fa951df78eb4dc8b4044a313
lzw920621 <1010616217@qq.com>
2019-08-01 08:44:15 +0800

    更新

---
+++ _876_链表的中间节点/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _876_链表的中间节点
+{
+    /*
+    给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+    如果有两个中间结点，则返回第二个中间结点。
+    
+    示例 1：
+
+    输入：[1,2,3,4,5]
+    输出：此列表中的结点 3 (序列化形式：[3,4,5])
+    返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+    注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+    ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+
+    示例 2：
+
+    输入：[1,2,3,4,5,6]
+    输出：此列表中的结点 4 (序列化形式：[4,5,6])
+    由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+   
+    提示：
+
+        给定链表的结点数介于 1 和 100 之间。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static ListNode MiddleNode(ListNode head)//快慢指针法
+        {
+            ListNode node1 = head;
+            ListNode node2 = head.next;
+            while(node2!=null)
+            {
+                node1 = node1.next;
+                node2 = node2.next;
+                if(node2!=null)
+                {
+                    node2 = node2.next;
+                }
+                else
+                {
+                    return node1;
+                }
+            }
+            return node1;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
