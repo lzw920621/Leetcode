b3c1935b3ae191f68f5e7c417d8fa3b36285c788
lzw920621 <1010616217@qq.com>
2019-08-10 14:22:30 +0800

    更新

---
+++ _155_最小栈/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _155_最小栈
+{
+    /*
+    设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+
+    push(x) -- 将元素 x 推入栈中。
+    pop() -- 删除栈顶的元素。
+    top() -- 获取栈顶元素。
+    getMin() -- 检索栈中的最小元素。
+
+    示例:
+
+    MinStack minStack = new MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    minStack.getMin();   --> 返回 -3.
+    minStack.pop();
+    minStack.top();      --> 返回 0.
+    minStack.getMin();   --> 返回 -2.
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/min-stack
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinStack minStack = new MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            int min=minStack.GetMin();
+        }
+    }
+
+    public class MinStack
+    {
+        int[] array;
+        int index;
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            array = new int[32];
+            index = -1;
+        }
+
+        public void Push(int x)
+        {
+            index++;
+            if(index>array.Length-1)
+            {
+                //扩容 数组长度增加一倍
+                Array.Resize(ref array, array.Length * 2);
+            }
+            array[index] = x;
+        }
+
+        public void Pop()
+        {
+            if(index>=0)
+            {
+                index--;
+            }
+        }
+
+        public int Top()
+        {
+            return array[index];
+        }
+
+        public int GetMin()
+        {
+            int smallest = array[0];
+            for (int i = 1; i <= index; i++)
+            {
+                if(array[i]<smallest)
+                {
+                    smallest = array[i];
+                }
+            }
+            return smallest;
+        }
+    }
+}
