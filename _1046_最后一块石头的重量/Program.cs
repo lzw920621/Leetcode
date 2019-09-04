2f41183c9fba4bb2db96e7d0e7fd53edc3725aae
lzw920621 <1010616217@qq.com>
2019-09-04 19:05:05 +0800

    更新

---
+++ _1046_最后一块石头的重量/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1046_最后一块石头的重量
+{
+    /*
+    有一堆石头，每块石头的重量都是正整数。
+
+    每一回合，从中选出两块最重的石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+
+        如果 x == y，那么两块石头都会被完全粉碎；
+        如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+
+    最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+
+    提示：
+        1 <= stones.length <= 30
+        1 <= stones[i] <= 1000
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/last-stone-weight
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int LastStoneWeight(int[] stones)
+        {
+            if (stones.Length == 1) return stones[0];
+            Array.Sort(stones);
+            while(stones[stones.Length-2]>0)
+            {
+                stones[stones.Length - 1] -= stones[stones.Length - 2];
+                stones[stones.Length - 2] = 0;
+                Array.Sort(stones);
+            }
+            return stones[stones.Length - 1];
+        }
+    }
+}
