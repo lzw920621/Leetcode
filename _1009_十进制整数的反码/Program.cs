3e0adc11b9e6ea89b6a92ded8c0543a3beba961e
lzw920621 <1010616217@qq.com>
2019-09-06 23:35:06 +0800

    更新

---
+++ _1009_十进制整数的反码/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1009_十进制整数的反码
+{
+    /*
+    每个非负整数 N 都有其二进制表示。例如， 5 可以被表示为二进制 "101"，11 可以用二进制 "1011" 表示，依此类推。注意，除 N = 0 外，任何二进制表示中都不含前导零。
+    二进制的反码表示是将每个 1 改为 0 且每个 0 变为 1。例如，二进制数 "101" 的二进制反码为 "010"。
+    给定十进制数 N，返回其二进制表示的反码所对应的十进制整数。
+
+    示例 1：
+
+    输入：5
+    输出：2
+    解释：5 的二进制表示为 "101"，其二进制反码为 "010"，也就是十进制中的 2 。
+
+    示例 2：
+
+    输入：7
+    输出：0
+    解释：7 的二进制表示为 "111"，其二进制反码为 "000"，也就是十进制中的 0 。
+
+    示例 3：
+
+    输入：10
+    输出：5
+    解释：10 的二进制表示为 "1010"，其二进制反码为 "0101"，也就是十进制中的 5 。
+
+    提示：
+
+        0 <= N < 10^9
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/complement-of-base-10-integer
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = BitwiseComplement3(5);
+        }
+
+        public static int BitwiseComplement(int N)
+        {
+            string str = Convert.ToString(N, 2);
+            int num = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                num = num << 1;
+                if (str[i]=='0')
+                {
+                    num += 1;
+                }                
+            }
+            return num;
+        }
+
+        public static int BitwiseComplement2(int N)
+        {
+            if (N == 0) return 1;
+            int num = 1;
+            while(num<=N)
+            {
+                num = num << 1;
+            }
+            return N ^ (num - 1);
+        }
+
+        public static int BitwiseComplement3(int N)
+        {            
+            int num = 2;
+            while (num <= N)
+            {
+                num = num << 1;
+            }
+            return num-N-1;
+        }
+
+        public static int BitwiseComplement4(int N)
+        {            
+            int num = 1;
+            while (num < N)
+            {
+                num = num << 1 + 1;
+            }
+            return N ^ num;
+        }
+    }
+}
---
+++ _1047_删除字符串中的所有相邻重复项/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1047_删除字符串中的所有相邻重复项
+{
+    /*
+    给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+
+    在 S 上反复执行重复项删除操作，直到无法继续删除。
+
+    在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+
+ 
+    示例：
+
+    输入："abbaca"
+    输出："ca"
+    解释：
+    例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+
+
+    提示：
+
+        1 <= S.length <= 20000
+        S 仅由小写英文字母组成。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = RemoveDuplicates("abbaca");
+        }
+
+        public static string RemoveDuplicates(string S)
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push(S[0]);
+            for (int i = 1; i < S.Length; i++)
+            {
+
+                if (stack.Count > 0 && S[i] == stack.Peek())
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(S[i]);
+                }
+            }
+            return new string(stack.Reverse().ToArray());
+        }
+
+        public static string RemoveDuplicates2(string S)
+        {            
+            StringBuilder sb = new StringBuilder();
+            sb.Append(S[0]);
+            for (int i = 1; i < S.Length; i++)
+            {
+                if(sb.Length>0 && S[i]==sb[sb.Length-1])
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                }
+                else
+                {
+                    sb.Append(S[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
