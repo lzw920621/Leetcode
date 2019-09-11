a6af319bea8ef52381f3677ca78c27924c974119
lzw920621 <1010616217@qq.com>
2019-09-11 11:45:03 +0800

    更新

--- 两数之和/Program.cs
+++ 两数之和/Program.cs
@@ -6,11 +6,45 @@ using System.Threading.Tasks;
 
 namespace _1_两数之和
 {
+    /*
+    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+    示例:
+
+    给定 nums = [2, 7, 11, 15], target = 9
+
+    因为 nums[0] + nums[1] = 2 + 7 = 9
+    所以返回 [0, 1]
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/two-sum
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
     class Program
     {
         static void Main(string[] args)
         {
 
         }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dic[target - nums[i]], i };
+                }
+                else if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+        
     }
 }
