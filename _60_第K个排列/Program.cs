56d2fc653920c624c1abe17e8f9be77ea29e250a
lzw920621 <1010616217@qq.com>
2019-10-03 19:09:20 +0800

    更新

--- _60_第K个排列/Program.cs
+++ _60_第K个排列/Program.cs
@@ -64,13 +64,13 @@ namespace _60_第K个排列
         }
         bool GetNext(int[] array)
         {
-            for (int i = array.Length-1; i >0; i--)
+            for (int i = array.Length - 1; i > 0; i--)
             {
-                if(array[i]>array[i-1])
+                if (array[i] > array[i - 1])
                 {
                     for (int j = array.Length - 1; j >= i; j--)
                     {
-                        if(array[j]> array[i - 1])
+                        if (array[j] > array[i - 1])
                         {
                             Swap(array, i - 1, j);
                             Reverse(array, i, array.Length - 1);
@@ -81,16 +81,16 @@ namespace _60_第K个排列
             }
             return false;
         }
-        void Swap(int[] array,int i,int j)
+        void Swap(int[] array, int i, int j)
         {
             int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
-        void Reverse(int[] array,int left,int right)
+        void Reverse(int[] array, int left, int right)
         {
             int temp;
-            while(left<right)
+            while (left < right)
             {
                 temp = array[left];
                 array[left] = array[right];
@@ -98,6 +98,57 @@ namespace _60_第K个排列
                 left++;
                 right--;
             }
+        }
+
+
+        public string GetPermutation2(int n, int k)
+        {
+            /*
+            用数学的方法来解, 因为数字都是从1开始的连续自然数, 排列出现的次序可以推
+            算出来, 对于n=4, k=15 找到k=15排列的过程:
+        
+            1 + 对2,3,4的全排列 (3!个)         
+            2 + 对1,3,4的全排列 (3!个)         3, 1 + 对2,4的全排列(2!个)
+            3 + 对1,2,4的全排列 (3!个)-------> 3, 2 + 对1,4的全排列(2!个)-------> 3, 2, 1 + 对4的全排列(1!个)-------> 3214
+            4 + 对1,2,3的全排列 (3!个)         3, 4 + 对1,2的全排列(2!个)         3, 2, 4 + 对1的全排列(1!个)
+        
+            确定第一位:
+                k = 14(从0开始计数)
+                index = k / (n-1)! = 2, 说明第15个数的第一位是3 
+                更新k
+                k = k - index*(n-1)! = 2
+            确定第二位:
+                k = 2
+                index = k / (n-2)! = 1, 说明第15个数的第二位是2
+                更新k
+                k = k - index*(n-2)! = 0
+            确定第三位:
+                k = 0
+                index = k / (n-3)! = 0, 说明第15个数的第三位是1
+                更新k
+                k = k - index*(n-3)! = 0
+            确定第四位:
+                k = 0
+                index = k / (n-4)! = 0, 说明第15个数的第四位是4
+            最终确定n=4时第15个数为3214 
+            */
+
+            StringBuilder sb = new StringBuilder();
+            // 候选数字
+            List<char> candidates = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            // 分母的阶乘数
+            int[] factorials = new int[] { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+            
+            k -= 1;
+            for (int i = n - 1; i >= 0; --i)
+            {
+                // 计算候选数字的index
+                int index = k / factorials[i];
+                sb.Append(candidates[index]);
+                candidates.RemoveAt(index);
+                k -= index * factorials[i];
+            }
+            return sb.ToString();
         }
     }
 }
