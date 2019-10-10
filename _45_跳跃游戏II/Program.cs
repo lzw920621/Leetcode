2b516d76276961e7ee70a0b1c195b2d58433091e
lzw920621 <1010616217@qq.com>
2019-10-10 22:26:56 +0800

    更新

---
+++ _45_跳跃游戏II/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45_跳跃游戏II
+{
+    /*
+        给定一个非负整数数组，你最初位于数组的第一个位置。
+    数组中的每个元素代表你在该位置可以跳跃的最大长度。
+    你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+    示例:
+
+    输入: [2,3,1,1,4]
+    输出: 2
+    解释: 跳到最后一个位置的最小跳跃数是 2。
+         从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+
+    说明:
+
+    假设你总是可以到达数组的最后一个位置。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/jump-game-ii
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+        */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+        }
+
+        //动态规划 部分测试用例超时
+        public int Jump(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dp[i] = int.MaxValue;
+            }
+            dp[0] = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {                
+                for (int j = 0; j < i; j++)
+                {
+                    if(j+nums[j]>=i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[j] + 1);                        
+                    }                    
+                }
+            }
+            return dp[nums.Length - 1];
+        }
+        //部分测试用例超时
+        public int Jump2(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dp[i] = int.MaxValue;
+            }
+            dp[0] = 0;
+            int reach = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {                
+                reach = i + nums[i];
+                for (int j = i+1; j <= reach && j<nums.Length; j++)
+                {
+                    dp[j] = Math.Min(dp[j], dp[i] + 1);
+                }
+            }
+            return dp[nums.Length - 1];
+        }
+    }
+}
---
+++ _87_扰乱字符串/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _87_扰乱字符串
+{
+    /*
+        给定一个字符串 s1，我们可以把它递归地分割成两个非空子字符串，从而将其表示为二叉树。
+    下图是字符串 s1 = "great" 的一种可能的表示形式。
+
+        great
+       /    \
+      gr    eat
+     / \    /  \
+    g   r  e   at
+               / \
+              a   t
+
+    在扰乱这个字符串的过程中，我们可以挑选任何一个非叶节点，然后交换它的两个子节点。
+
+    例如，如果我们挑选非叶节点 "gr" ，交换它的两个子节点，将会产生扰乱字符串 "rgeat" 。
+
+        rgeat
+       /    \
+      rg    eat
+     / \    /  \
+    r   g  e   at
+               / \
+              a   t
+
+    我们将 "rgeat” 称作 "great" 的一个扰乱字符串。
+
+    同样地，如果我们继续交换节点 "eat" 和 "at" 的子节点，将会产生另一个新的扰乱字符串 "rgtae" 。
+
+        rgtae
+       /    \
+      rg    tae
+     / \    /  \
+    r   g  ta  e
+           / \
+          t   a
+
+    我们将 "rgtae” 称作 "great" 的一个扰乱字符串。
+
+    给出两个长度相等的字符串 s1 和 s2，判断 s2 是否是 s1 的扰乱字符串。
+
+    示例 1:
+
+    输入: s1 = "great", s2 = "rgeat"
+    输出: true
+
+    示例 2:
+
+    输入: s1 = "abcde", s2 = "caebd"
+    输出: false
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/scramble-string
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = new Program().IsScramble("great","rgtae");
+        }
+        //string s1, s2;
+        public bool IsScramble(string s1, string s2)
+        {
+            if (s1 == s2) return true;
+            if (s1.Length != s2.Length) return false;
+
+            //判断两个字符串每个字母出现的次数是否一致
+            int[] letters = new int[26];
+            for (int i = 0; i < s1.Length; i++)
+            {
+                letters[s1[i] - 'a']++;
+                letters[s2[i] - 'a']--;
+            }
+            //如果两个字符串的字母出现不一致直接返回 false
+            for (int i = 0; i < 26; i++)
+            {
+                if (letters[i] != 0)
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 1; i < s1.Length; i++)
+            {
+                string s11 = s1.Substring(0, i);
+                string s12 = s1.Substring(i, s1.Length - i);
+                string s21 = s2.Substring(0, i);
+                string s22 = s2.Substring(i, s2.Length - i);
+                if (IsScramble(s11, s21) && IsScramble(s12, s22)) return true;
+                string s2_1 = s2.Substring(s2.Length - i, i);
+                string s2_2 = s2.Substring(0, s2.Length - i);
+                if (IsScramble(s11, s2_1) && IsScramble(s12, s2_2)) return true;
+            }
+            return false;
+        }
+
+       
+    }
+}
