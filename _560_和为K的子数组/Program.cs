4154b01c5873b6833a665bb05aa59c3205d8e3c0
lzw920621 <1010616217@qq.com>
2019-10-19 09:06:20 +0800

    更新

---
+++ _560_和为K的子数组/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _560_和为K的子数组
+{
+    /*
+        给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+
+    示例 1 :
+
+    输入:nums = [1,1,1], k = 2
+    输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+
+    说明 :
+
+        数组的长度为 [1, 20,000]。
+        数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = SubarraySum(new int[] { -1, -1, 1 }, 0);
+        }
+
+        public static int SubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            dic[0] = 1;
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {                
+                sum += nums[i];
+
+                if (dic.ContainsKey(sum - k))
+                {
+                    count += dic[sum - k];
+                }
+
+                if (dic.ContainsKey(sum))
+                {
+                    dic[sum]++;
+                }
+                else
+                {
+                    dic[sum] = 1;
+                }
+            }
+            return count;
+        }
+    }
+}
