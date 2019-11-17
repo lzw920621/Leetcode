13cf884cfea51ddd569fa687ad5177ba2c5fb829
lzw920621 <1010616217@qq.com>
2019-11-17 22:49:02 +0800

    更新

---
+++ _890_查找和替换模式/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _890_查找和替换模式
+{
+    /*
+        你有一个单词列表 words 和一个模式  pattern，你想知道 words 中的哪些单词与模式匹配。
+
+    如果存在字母的排列 p ，使得将模式中的每个字母 x 替换为 p(x) 之后，我们就得到了所需的单词，那么单词与模式是匹配的。
+
+    （回想一下，字母的排列是从字母到字母的双射：每个字母映射到另一个字母，没有两个字母映射到同一个字母。）
+
+    返回 words 中与给定模式匹配的单词列表。
+
+    你可以按任何顺序返回答案。
+
+ 
+
+    示例：
+
+    输入：words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+    输出：["mee","aqq"]
+    解释：
+    "mee" 与模式匹配，因为存在排列 {a -> m, b -> e, ...}。
+    "ccc" 与模式不匹配，因为 {a -> c, b -> c, ...} 不是排列。
+    因为 a 和 b 映射到同一个字母。
+
+ 
+
+    提示：
+
+        1 <= words.length <= 50
+        1 <= pattern.length = words[i].length <= 20
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/find-and-replace-pattern
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> list = new Program().FindAndReplacePattern(new string[] { "abc", "deq", "mee", "aqq", "dkd", "ccc" }, "abb");
+        }
+
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            IList<string> list = new List<string>();
+
+            Dictionary<char, List<int>> dic = new Dictionary<char, List<int>>();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if(dic.ContainsKey(pattern[i]))
+                {
+                    dic[pattern[i]].Add(i);
+                }
+                else
+                {
+                    dic[pattern[i]] = new List<int> { i };
+                }
+            }
+            bool flag;
+            foreach (var word in words)
+            {
+                flag = true;
+                Dictionary<char, List<int>> tempDic = new Dictionary<char, List<int>>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (tempDic.ContainsKey(word[i]))
+                    {
+                        tempDic[word[i]].Add(i);
+                    }
+                    else
+                    {
+                        tempDic[word[i]] = new List<int> { i };                        
+                    }
+                }
+                if(tempDic.Count!=dic.Count)
+                {
+                    continue;
+                }
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if(!tempDic[word[i]].SequenceEqual(dic[pattern[i]]))
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag==true)
+                {
+                    list.Add(word);
+                }
+            }
+
+            return list;
+        }
+    }
+}
